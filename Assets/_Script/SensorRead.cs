c685d3852e0467810dd50819f55100e3ef50124f
phlegmatic-tuna <phlegmatictuna@gmail.com>
2024-03-29 18:04:37 -0700

    add sensor read script

---
+++ Assets/_Script/SensorRead.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+using UnityEditor;
+using TMPro;
+
+public class SensorRead : MonoBehaviour
+{
+    UduinoManager uduinoManager;
+    public int forceRead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uduinoManager = UduinoManager.Instance;
+        uduinoManager.pinMode(AnalogPin.A0, PinMode.Input);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(ReadValueTenTimesPerSec());     
+    }
+    IEnumerator ReadValueTenTimesPerSec(){
+        while(true){
+            forceRead = uduinoManager.analogRead(AnalogPin.A0);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
